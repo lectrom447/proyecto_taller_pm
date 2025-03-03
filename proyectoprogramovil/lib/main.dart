907642e2374830103fca79374f940fd93a91c5d9
Emanuel Mendez <emanuelfmendez@outlook.com>
2025-03-02 22:02:46 -0600

    Added firebase connection

--- proyectoprogramovil/lib/main.dart
+++ proyectoprogramovil/lib/main.dart
@@ -1,12 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:proyectoprogramovil/firebase_options.dart';
 import 'package:proyectoprogramovil/pages/login_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -17,7 +21,7 @@ class MyApp extends StatelessWidget {
           foregroundColor: Colors.white,
         ),
       ),
-      home: const LoginPage(),  
+      home: const LoginPage(),
     );
   }
 }
